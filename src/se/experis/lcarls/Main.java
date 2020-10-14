e0735d57ce5cbaed40691d21f8bae64b76e4543e
ludwigcarlsson <zhendiac@gmail.com>
2020-10-14 09:21:06 +0200

    A working luhn example

--- src/se/experis/lcarls/Main.java
+++ src/se/experis/lcarls/Main.java
@@ -17,15 +17,18 @@ public class Main {
             return false;
         }
         char checkDigit = input.charAt(input.length() -1);
-        String digit = checkLuhn(input.substring(0, input.length()-1), checkDigit);
-
+        System.out.println("Input: "+input.substring(0, input.length()-1) +" "+ checkDigit);
+        System.out.println("Provided: "+checkDigit);
+        String digit = calCheckDigit(input.substring(0, input.length()-1));
+        System.out.println("Expected: "+digit.charAt(0)+"\n");
         return checkDigit == digit.charAt(0);
     }
 
-    public static String checkLuhn(String input, Character checkDigit) {
+    public static String calCheckDigit(String input) {
         if (input == null) {
             return null;
         }
+
         String digit;
 
         int[] digits = new int[input.length()];
@@ -36,20 +39,16 @@ public class Main {
 
         for (int i = digits.length-1; i >= 0; i -= 2) {
             digits[i] += digits[i];
-            if (digits[i] > 9) {
+            if (digits[i] >= 10) {
                 digits[i] = digits[i] - 9;
             }
         }
         int sum = 0;
-        for (int j : digits) {
-            sum += j;
+        for (int j = 0; j < digits.length; j++) {
+            sum += digits[j];
         }
         sum=sum*9;
         digit = sum + "";
-
-        System.out.println("Input: "+digit.substring(digit.length() - 1));
-
-        System.out.println("\nChecksum: "+((sum%10) == checkDigit ? "Valid" : "Invalid"));
 
         return digit.substring(digit.length()-1);
 
